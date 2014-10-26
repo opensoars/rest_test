0ec632cd85997dda7590272563364e9fcc854462
opensoars <sam_1700@hotmail.com>
2014-10-26 14:21:49 +0100

    Serves polls with GET /api/polls

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-require('./lib/server.js');
+process.DIR = __dirname;
+process.PORT = 80;
+
+/**
+ * Let's create some sample polls to serve
+ */
+
+process.POLL_COL = require('./lib/poll_col');
+
+
+require('./lib/server');
